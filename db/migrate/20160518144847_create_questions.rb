ddc3c93e8a713feb52a228c55a786a770c9b8255
torihuang <victoriahuang20@gmail.com>
2016-05-18 10:04:58 -0500

    Create migrations and seed data

--- db/migrate/20160518144847_create_questions.rb
+++ db/migrate/20160518144847_create_questions.rb
@@ -1,6 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.integer :game_id
+      t.string  :query
+      t.string  :answer
+      t.attachment :image
+
+      t.integer :video_id
 
       t.timestamps null: false
     end
