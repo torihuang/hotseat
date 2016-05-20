c49223baa27cd1a47c3ff983eaa763fdc79e5bd5
torihuang <victoriahuang20@gmail.com>
2016-05-20 07:22:56 -0500

    Add images and schema file

---
+++ app/assets/javascripts/splashpage.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // var imgs = ['laughing3.jpg', 'laughing4.jpg', 'laughing5.jpg', 'laughing2.jpg'];
+  // var index = 0;
+
+  // var myInterval = setInterval(function () {
+  //     $('#banner-image').remove();
+  //     $('#scroll-bar').append("<img src='/assets/" + imgs[index] + "', id: 'banner-image'>");
+  //     index += 1;
+  //     if (index == imgs.length) {
+  //       index = 0;
+  //     }
+  //   },10000);
+})
---
+++ app/helpers/session_helper.rb
@@ -0,0 +1,15 @@
+module SessionHelper
+
+  def logged_in?
+    return !!session['user_id']
+  end
+
+  def current_user
+    if logged_in?
+      return User.find_by(id: session['user_id'])
+    else
+      return nil
+    end
+  end
+
+end
